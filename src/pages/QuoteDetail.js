86d82cfead06b388f0f7f58521feef9bc6b9d858
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-28 17:12:41 +0900

    #281 /quotesのview fullscreenボタン押下でQuoteDetail.jsを表示する

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -4,15 +4,27 @@
 import { Fragment } from 'react';
 import { useParams, Route } from 'react-router';
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+// のちにbackend serverに保存するデータ
+const DUMMY_QUOTES = [
+  { id: 'p1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'p2', author: 'Maximilian', text: 'Learning React is great!' },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.auther} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         {/* alternatively, since we're defining a route here (not a link), also can set <path='/quotes/:quoteId/comments'> */}
         <Comments />
