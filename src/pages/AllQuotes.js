07c25d62c2992085a177ccca0b7018bdb6701f58
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-28 16:28:23 +0900

    added dummy-quotes data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,6 +1,19 @@
 // this component holds all the quotes displayed on the screen
+
+import QuoteList from '../components/quotes/QuoteList';
+
+// のちにbackend serverに保存するデータ
+const DUMMY_QUOTES = [
+  { id: 'p1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'p2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes</h1>;
+  return (
+    // DUMMY_QUOTESをQuoteListにprops.quotesとして送る
+    // ViewFullscreenボタン(QuoteItem.js)押下でQuoteDetail.jsにrouterしたい
+    <QuoteList quotes={DUMMY_QUOTES} />
+  );
 };
 
 export default AllQuotes;
