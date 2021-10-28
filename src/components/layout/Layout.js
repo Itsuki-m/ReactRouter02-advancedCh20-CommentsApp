6665ff4f05d960a2c1cf6b9a51045c56dd0d8bdd
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-28 16:06:25 +0900

    added a layout wrapper component #279

---
+++ src/components/layout/Layout.js
@@ -0,0 +1,17 @@
+// to render the MainNavigation side by side with the actual page content
+
+import { Fragment } from 'react';
+
+import classes from './Layout.module.css';
+import MainNavigation from './MainNavigation';
+
+const Layout = (props) => {
+  return (
+    <Fragment>
+      <MainNavigation />
+      <main className={classes.main}>{props.children}</main>
+    </Fragment>
+  );
+};
+
+export default Layout;
