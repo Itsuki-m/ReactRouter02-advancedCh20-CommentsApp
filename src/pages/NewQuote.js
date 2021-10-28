fd6b22fe7e7a2c3011d97cb5ada5a3edba46ec79
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-28 18:25:31 +0900

    Add Quoteボタン(/new-quote)押下したら(/quotes)に戻る added useHistory #283

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,11 +1,17 @@
 //NewQuote adding a brand new quote
 // this page should display a form, which allows to add a new quote
 
+import { useHistory } from 'react-router-dom';
+
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push('/quotes');
   };
   return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
