9a5511adc410b71ee7d9cb05b21270e964fc50ee
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-28 15:13:18 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/" exact>
+        <Redirect to="/quotes" />
+      </Route>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
+
+// AllQuotes
+//QuoteDetailPage when we click on a singleQuote, then we go to this page
+//NewQuote adding a brand new quote
 
 export default App;
