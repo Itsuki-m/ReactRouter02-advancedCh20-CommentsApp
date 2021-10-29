ca3de7252c6e187de2a3f159b97118521b43368b
Itsuki-m <mizuharu_sawaka14@yahoo.co.jp>
2021-10-29 12:20:24 +0900

    firebase新しいのに変更

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,4 @@
-const FIREBASE_DOMAIN =
-  'https://react-router-sc20-287-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://multipagespa-ch20-default-rtdb.firebaseio.com';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
